7c324dc10ffe8d420bcf6cdca6b6f4752f45edae
Franco Navarro <francotel@francotel.lan>
2024-10-12 13:08:04 -0500

    add sudo docker v2

--- install-tools.sh
+++ install-tools.sh
@@ -68,10 +68,25 @@ install_docker() {
     # Agrega el usuario actual al grupo docker
     sudo usermod -aG docker $USER
 
-    # Usa newgrp para evitar tener que hacer logout y login
-    newgrp docker << EOF
-    sudo curl https://raw.githubusercontent.com/jesseduffield/lazydocker/master/scripts/install_update_linux.sh | bash
-EOF
+    # Directorio destino (puedes cambiarlo si es necesario)
+    DIR="${DIR:-"$HOME/.local/bin"}"
+
+    # Determina la arquitectura del sistema
+    ARCH=\$(uname -m)
+    case \$ARCH in
+        i386|i686) ARCH=x86 ;;
+        armv6*) ARCH=armv6 ;;
+        armv7*) ARCH=armv7 ;;
+        aarch64*) ARCH=arm64 ;;
+    esac
+
+    # Obtén la última versión disponible de LazyDocker
+    GITHUB_LATEST_VERSION=\$(curl -L -s -H 'Accept: application/json' https://github.com/jesseduffield/lazydocker/releases/latest | sed -e 's/.*"tag_name":"\([^"]*\)".*/\1/')
+    GITHUB_FILE="lazydocker_\${GITHUB_LATEST_VERSION//v/}_\$(uname -s)_\${ARCH}.tar.gz"
+    GITHUB_URL="https://github.com/jesseduffield/lazydocker/releases/download/\${GITHUB_LATEST_VERSION}/\${GITHUB_FILE}"
+
+    # Descarga y descomprime LazyDocker
+    sudo curl -L -o lazydocker.tar.gz \$GITHUB_URL
 
     echo "Docker ha sido instalado y LazyDocker ha sido configurado correctamente."
 }
